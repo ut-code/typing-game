160a4ea82e45e0e1022353989529bfe4557f12ad
chvmvd <chvmvd@users.noreply.github.com>
2022-08-31 23:10:52 +0900

    データベースから問題をとれるようにした。

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
+// Prisma関係
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-const express = require("express")
+const express = require("express");
 const fs = require("fs");
 
 const app = express();
 
-app.use(express.static("static"))
-
-app.listen(3000);
+app.use(express.static("static"));
+app.use(express.json());
 
 // データベースからPrismaで問題をとってくる
-async function getQuestions() {
+app.post("/questions", async (request, response) => {
     // questionsに問題が配列の形で入っている。
     const questions = await (await prisma.questions.findMany({
         select: {
             // id: true,
             question: true,
         },
-    })).map((data)=>(data.question));
-}
-// exports.questions_list = getQuestions();
-getQuestions();
+    })).map((data) => (data.question));
+    // JSON形式でscript.jsに送信
+    response.json(questions);
+});
+
+app.listen(3000);
