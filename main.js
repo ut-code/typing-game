1fe6e6e6227ecb617c938be0828943bd96d9c19f
baar-vendel <fhinnoigsr@gmail.com>
2022-08-31 12:37:46 +0900

    ElephantSQLでデータベースを作成し、試しに問題のテーブルを作成。console.log()で確認できる。Prismaで操作してscript.jsのいち行目を書き換えたかったが叶わず。

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
-const express=require("express")
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
+const express=require("express")
+const fs = require("fs");
 const app=express();
 app.use(express.static("static"))
 app.listen(3000);
+
+// データベースからPrismaで問題をとってくる
+async function getQuestions() {
+    const questions = await prisma.questions.findMany({
+        select: {
+            // id: true,
+            question: true,
+        },
+    });
+    let l = []
+    for (let i=0; i<questions.length; i++){
+        l.push(questions[i].question)
+    }
+    console.log(l)
+    return l
+}
+// exports.questions_list = getQuestions();
+getQuestions();
