7bc04bfed7fa282a9828470404147e6e5ae22ddd
chvmvd <chvmvd@users.noreply.github.com>
2022-09-02 17:40:30 +0900

    微修正

--- static/script.js
+++ static/script.js
@@ -1,4 +1,4 @@
-let questions = []; // 問題
+const questions = []; // 問題
 // 問題をquestionsに格納
 async function getQuestions() {
     // JSON形式でmain.jsから受信
@@ -8,20 +8,48 @@ async function getQuestions() {
     });
     // テキストを取り出し、objectに
     questions = JSON.parse(await response.text());
+
+
+    // 配列をシャッフルする。
+    function shuffle(array) {
+        for (let i = 0; i < array.length; i++) {
+            let j = Math.floor(Math.random() * array.length);
+            let tmp = array[i];
+            array[i] = array[j];
+            array[j] = tmp;
+        }
+        return array;
+    }
+    questions = shuffle(questions);
+
+    document.addEventListener('keydown', (event) => {// 何かキーが押されたら、実行 https://developer.mozilla.org/ja/docs/Web/API/Element/keydown_event
+        if (event.key === questions[word_num][cnt]) {// 正答時
+            answer = answer + event.key;
+            cnt++;
+            correct++;
+        } else if (65 <= event.keyCode && event.keyCode <= 90) {// 不正解の時
+            miss++;
+        }
+        if (cnt == questions[word_num].length) {// 次の問題へ
+            word_num++;
+            answer = "";
+            cnt = 0;
+            if (word_num === questions.length) document.write("終了! 時間" + time + "秒");
+        }
+        if (event.key === " " && isStarted === false) {// スペースが押されたら、時間計測
+            isStarted = true;
+            setInterval(() => {
+                time++;
+                document.getElementById("time").textContent = "経過時間：" + time + "秒";
+            }, 1000);
+        }
+        document.getElementById("question").textContent = questions[word_num];
+        document.getElementById("answer").textContent = answer;
+        document.getElementById("miss").textContent = "ミスタイプ数：" + miss + "回";
+        document.getElementById("correct").textContent = "正しいタイプ数：" + correct + "回";
+    });
 };
 getQuestions();
-
-// 配列をシャッフルする。
-function shuffle(array) {
-    for (let i = 0; i < array.length; i++) {
-        let j = Math.floor(Math.random() * array.length);
-        let tmp = array[i];
-        array[i] = array[j];
-        array[j] = tmp;
-    }
-    return array;
-}
-questions = shuffle(questions);
 
 let answer = "";// 現在の到達状況
 let word_num = 0;// 何問目か
@@ -35,30 +63,3 @@ let time = 0;// 時間
 document.getElementById("miss").textContent = "ミスタイプ数：" + miss + "回";
 document.getElementById("correct").textContent = "正しいタイプ数：" + correct + "回";
 document.getElementById("time").textContent = "経過時間：" + time + "秒";
-
-document.addEventListener('keydown', (event) => {// 何かキーが押されたら、実行 https://developer.mozilla.org/ja/docs/Web/API/Element/keydown_event
-    if (event.key === questions[word_num][cnt]) {// 正答時
-        answer = answer + event.key;
-        cnt++;
-        correct++;
-    } else if (65 <= event.keyCode && event.keyCode <= 90) {// 不正解の時
-        miss++;
-    }
-    if (cnt == questions[word_num].length) {// 次の問題へ
-        word_num++;
-        answer = "";
-        cnt = 0;
-        if (word_num === questions.length) document.write("終了! 時間" + time + "秒");
-    }
-    if (event.key === " " && isStarted === false) {// スペースが押されたら、時間計測
-        isStarted = true;
-        setInterval(() => {
-            time++;
-            document.getElementById("time").textContent = "経過時間：" + time + "秒";
-        }, 1000);
-    }
-    document.getElementById("question").textContent = questions[word_num];
-    document.getElementById("answer").textContent = answer;
-    document.getElementById("miss").textContent = "ミスタイプ数：" + miss + "回";
-    document.getElementById("correct").textContent = "正しいタイプ数：" + correct + "回";
-});
