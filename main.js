da336b04ffdb122d33760a9e06623a8186986e64
baar-vendel <fhinnoigsr@gmail.com>
2022-09-04 18:23:13 +0900

    “ejsをインストール,データベースにスコア記録用のテーブル作成,リザルト画面とランキングを表示するresults.ejsを作成”

--- main.js
+++ main.js
@@ -3,24 +3,47 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const express = require("express");
+const ejs = require("ejs");
 const fs = require("fs");
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("static"));
 app.use(express.json());
 
+
 // データベースからPrismaで問題をとってくる
+async function getQuestion() {
+    const records = await prisma.questions.findMany();
+    console.log(records);
+    return records;
+}
+
+// 
 app.post("/questions", async (request, response) => {
     // questionsに問題が配列の形で入っている。
-    const questions = await (await prisma.questions.findMany({
-        select: {
-            // id: true,
-            question: true,
-        },
-    })).map((data) => (data.question));
+    const records = await getQuestion();
+    const questions = records.map((data) => (data.question));
     // JSON形式でscript.jsに送信
     response.json(questions);
+});
+
+
+// データベースからPrismaでランキングをとってくる
+async function getRanking() {
+    const records = await prisma.ranking.findMany();
+    console.log(records)
+    return records;
+}
+
+app.get("/results", async (request, response) => {
+    const records = await getRanking();
+    const template = fs.readFileSync("results.ejs", "utf-8");
+    const html = ejs.render(template, {
+        listItems: records,
+    });
+    response.send(html);
 });
 
 app.listen(3000);
