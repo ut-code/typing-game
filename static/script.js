5614bd6aa311d085eec97639a4fd6db317093ed0
chvmvd <chvmvd@users.noreply.github.com>
2022-09-05 08:47:39 +0900

    fix:Bug fixed(非同期関数の処理を修正)

--- static/script.js
+++ static/script.js
@@ -16,6 +16,34 @@ async function getQuestions() {
         [questions[i], questions[j]] = [questions[j], questions[i]]
     }
     // questions = array;
+
+
+    document.addEventListener('keydown', (event) => {// 何かキーが押されたら、実行 https://developer.mozilla.org/ja/docs/Web/API/Element/keydown_event
+        if (event.key === questions[word_num][cnt]) {// 正答時
+            answer = answer + event.key;
+            cnt++;
+            correct++;
+        } else if (65 <= event.keyCode && event.keyCode <= 90) {// 不正解の時
+            miss++;
+        }
+        if (cnt == questions[word_num].length) {// 次の問題へ
+            word_num++;
+            answer = "";
+            cnt = 0;
+            if (word_num === questions.length) document.write("終了! 時間" + time + "秒");
+        }
+        if (event.key === " " && isStarted === false) {// スペースが押されたら、時間計測
+            isStarted = true;
+            setInterval(() => {
+                time++;
+                document.getElementById("time").textContent = "経過時間：" + time + "秒";
+            }, 1000);
+        }
+        document.getElementById("question").textContent = questions[word_num];
+        document.getElementById("answer").textContent = answer;
+        document.getElementById("miss").textContent = "ミスタイプ数：" + miss + "回";
+        document.getElementById("correct").textContent = "正しいタイプ数：" + correct + "回";
+    });
 };
 getQuestions();
 
@@ -31,30 +59,3 @@ let time = 0;// 時間
 document.getElementById("miss").textContent = "ミスタイプ数：" + miss + "回";
 document.getElementById("correct").textContent = "正しいタイプ数：" + correct + "回";
 document.getElementById("time").textContent = "経過時間：" + time + "秒";
-
-document.addEventListener('keydown', (event) => {// 何かキーが押されたら、実行 https://developer.mozilla.org/ja/docs/Web/API/Element/keydown_event
-    if (event.key === questions[word_num][cnt]) {// 正答時
-        answer = answer + event.key;
-        cnt++;
-        correct++;
-    } else if (65 <= event.keyCode && event.keyCode <= 90) {// 不正解の時
-        miss++;
-    }
-    if (cnt == questions[word_num].length) {// 次の問題へ
-        word_num++;
-        answer = "";
-        cnt = 0;
-        if (word_num === questions.length) document.write("終了! 時間" + time + "秒");
-    }
-    if (event.key === " " && isStarted === false) {// スペースが押されたら、時間計測
-        isStarted = true;
-        setInterval(() => {
-            time++;
-            document.getElementById("time").textContent = "経過時間：" + time + "秒";
-        }, 1000);
-    }
-    document.getElementById("question").textContent = questions[word_num];
-    document.getElementById("answer").textContent = answer;
-    document.getElementById("miss").textContent = "ミスタイプ数：" + miss + "回";
-    document.getElementById("correct").textContent = "正しいタイプ数：" + correct + "回";
-});
