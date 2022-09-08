10894fd203d0bf50ee068233954cc5ca152fcb77
chvmvd <chvmvd@users.noreply.github.com>
2022-09-08 19:04:49 +0900

    change:ファイル入力を関数化

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,28 @@ import React, { useState } from "react";
 
 let defaultKeyLayout: any = qwerty;
 
-const makeJSONFile=(object:object,fileName:string)=>{
+const makeJSONFile = (object: object, fileName: string) => {
   const blob = new Blob([JSON.stringify(object)], { type: "application/json" });
   const a = document.createElement("a");
   a.href = URL.createObjectURL(blob);
-  a.download =fileName+".json";
+  a.download = fileName + ".json";
   a.click();
   URL.revokeObjectURL(a.href);
+}
+
+const ReadJSONFile = (props: { f: (x: object) => void }) => {
+  return (
+    <input type="file" accept=".json" onChange={async (e) => {
+      if (e.target.files !== null) {
+        const reader = new FileReader();
+        const file = e.target.files[0];
+        reader.readAsText(file, "utf-8");
+        reader.onload = () => {
+          props.f(JSON.parse(reader.result as string));
+        }
+      }
+    }} />
+  );
 }
 
 export default function App() {
@@ -18,17 +33,10 @@ export default function App() {
   const [fileName, setFileName] = useState<string>("");
   return (
     <>
-      <input type="file" accept=".json" onChange={async (e) => {
-        if (e.target.files !== null) {
-          const reader = new FileReader();
-          const file = e.target.files[0];
-          await reader.readAsText(file, "utf-8");
-          reader.onload = () => {
-            defaultKeyLayout = JSON.parse(reader.result as string);
-            setKeys(eventCode.map((code) => (defaultKeyLayout[code])));
-          }
-        }
-      }} />
+      <ReadJSONFile f={(x) => {
+        defaultKeyLayout = x;
+        setKeys(eventCode.map((code) => (defaultKeyLayout[code])));
+      }} ></ReadJSONFile>
       {eventCode.map((code, i) => (
         <li key={code}>{code}
           <input
@@ -38,13 +46,13 @@ export default function App() {
             }}></input>
         </li>
       ))}
-      <input type="text" placeholder="ファイル名を入力してください。" value={fileName} onChange={(e)=>{setFileName(e.target.value)}} />
+      <input type="text" placeholder="ファイル名を入力してください。" value={fileName} onChange={(e) => { setFileName(e.target.value) }} />
       <button onClick={() => {
         let result = {};
         for (let i = 0; i < keys.length; i++) {
           Object.assign(result, { [eventCode[i]]: keys[i] });
         }
-        makeJSONFile(result,fileName);
+        makeJSONFile(result, fileName);
       }}>確定</button>
     </>
   );
