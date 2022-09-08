9b40badc9dcf1c74e0692e68282a86bd4b6d9d6a
chvmvd <chvmvd@users.noreply.github.com>
2022-09-08 23:19:30 +0900

    add:型定義を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import eventCode from "./eventCode.json";
 import qwerty from "./qwerty.json";
+import EventCode from "./eventCode.json";
 import React, { useState } from "react";
 
-let defaultKeyLayout: any = qwerty;
+let defaultKeyLayout: EventCode = qwerty;
 
 const makeJSONFile = (object: object, fileName: string) => {
   const blob = new Blob([JSON.stringify(object)], { type: "application/json" });
@@ -11,49 +12,70 @@ const makeJSONFile = (object: object, fileName: string) => {
   a.download = fileName + ".json";
   a.click();
   URL.revokeObjectURL(a.href);
-}
+};
 
-const ReadJSONFile = (props: { f: (x: object) => void }) => {
+const ReadJSONFile = (props: { f: (x: EventCode) => void }) => {
   return (
-    <input type="file" accept=".json" onChange={async (e) => {
-      if (e.target.files !== null) {
-        const reader = new FileReader();
-        const file = e.target.files[0];
-        reader.readAsText(file, "utf-8");
-        reader.onload = () => {
-          props.f(JSON.parse(reader.result as string));
+    <input
+      type="file"
+      accept=".json"
+      onChange={async (e) => {
+        if (e.target.files !== null) {
+          const reader = new FileReader();
+          const file = e.target.files[0];
+          reader.readAsText(file, "utf-8");
+          reader.onload = () => {
+            props.f(JSON.parse(reader.result as string));
+          };
         }
-      }
-    }} />
+      }}
+    />
   );
-}
+};
 
 export default function App() {
-  const [keys, setKeys] = useState<string[]>(eventCode.map((code) => (defaultKeyLayout[code])));
+  const [keys, setKeys] = useState<string[]>(
+    eventCode.map((code) => defaultKeyLayout[code])
+  );
   const [fileName, setFileName] = useState<string>("");
   return (
     <>
-      <ReadJSONFile f={(x) => {
-        defaultKeyLayout = x;
-        setKeys(eventCode.map((code) => (defaultKeyLayout[code])));
-      }} ></ReadJSONFile>
+      <ReadJSONFile
+        f={(x: EventCode) => {
+          defaultKeyLayout = x;
+          setKeys(eventCode.map((code) => defaultKeyLayout[code]));
+        }}
+      ></ReadJSONFile>
       {eventCode.map((code, i) => (
-        <li key={code}>{code}
+        <li key={code}>
+          {code}
           <input
             value={keys[i]}
             onChange={(e) => {
               setKeys(keys.map((key, j) => (i === j ? e.target.value : key)));
-            }}></input>
+            }}
+          ></input>
         </li>
       ))}
-      <input type="text" placeholder="ファイル名を入力してください。" value={fileName} onChange={(e) => { setFileName(e.target.value) }} />
-      <button onClick={() => {
-        let result = {};
-        for (let i = 0; i < keys.length; i++) {
-          Object.assign(result, { [eventCode[i]]: keys[i] });
-        }
-        makeJSONFile(result, fileName);
-      }}>確定</button>
+      <input
+        type="text"
+        placeholder="ファイル名を入力してください。"
+        value={fileName}
+        onChange={(e) => {
+          setFileName(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          let result = {};
+          for (let i = 0; i < keys.length; i++) {
+            Object.assign(result, { [eventCode[i]]: keys[i] });
+          }
+          makeJSONFile(result, fileName);
+        }}
+      >
+        確定
+      </button>
     </>
   );
 }
