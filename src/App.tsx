486b0e50f8748ae6755798ff218c8d2b4cbdf1ee
chvmvd <chvmvd@users.noreply.github.com>
2022-09-11 15:28:53 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import eventCode from "./eventCode.json"
+import qwerty from "./qwerty.json"
+import dvorak from "./dvorak.json"
+import "./App.css"
+
+const newRow = [
+  "Backquote",
+  "Tab",
+  "CapsLock",
+  "ShiftLeft",
+  "ControlLeft",
+];
+
+const result = [];
+let cnt = 0;
+for (let i = 0; i <= newRow.length; i++) {
+  const tmp = [];
+  while (eventCode[cnt] !== newRow[i]) {
+    tmp.push(eventCode[cnt]);
+    cnt++;
+  }
+  result.push(tmp);
+}
+
+export default function App() {
+  return (
+    <>
+      <div id="keyboard">
+        {result.map((row,i)=>(
+          <div id="row" key={i}>{row.map((code)=>(<div className="code" key={code}>{qwerty[code]}</div>))}</div>
+        ))}
+      </div>
+    </>
+  );
+}
