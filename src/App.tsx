f502a37d7cceb762be0d42dde8df9e71d90c8fd4
chvmvd <chvmvd@users.noreply.github.com>
2022-09-12 17:54:52 +0900

    jis109を使って表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,27 @@
 import eventCode from "./eventCode.json";
 import qwerty from "./qwerty.json";
 import dvorak from "./dvorak.json";
+import jis109 from "./JIS109.json";
 import "./App.css";
-
-const newRow = ["Backquote", "Tab", "CapsLock", "ShiftLeft", "ControlLeft"];
-
-const result = [];
-let cnt = 0;
-for (let i = 0; i <= newRow.length; i++) {
-  const tmp = [];
-  while (eventCode[cnt] !== newRow[i]) {
-    tmp.push(eventCode[cnt]);
-    cnt++;
-  }
-  result.push(tmp);
-}
 
 export default function App() {
   return (
     <>
       <div id="keyboard">
-        {result.map((row, i) => (
-          <div id="row" key={i}>
-            {row.map((code) => (
-              <div className="code" key={code}>
-                {qwerty[code]}
-              </div>
-            ))}
+        {eventCode.map((code) => (
+          <div
+            id={code}
+            className="key"
+            style={{
+              position: "absolute",
+              top: jis109[code].row * 10 + "%",
+              left: jis109[code].column * 10 + "%",
+              width: jis109[code].width / 10 + "%",
+            }}
+          >
+            {code}
           </div>
         ))}
-      </div>
-      <div id="hoge">
-        <div style={{ position: "relative", left: "10px" }}>aaa</div>
-        <div>bbb</div>
-        <div>ccc</div>
-        <span className="br" />
-        <div style={{ position: "relative", left: "10px" }}>ddd</div>
-        <div>eee</div>
-        <div>fff</div>
       </div>
     </>
   );
