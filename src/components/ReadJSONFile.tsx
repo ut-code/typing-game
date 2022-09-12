9c96c4b615f7862c99b914288172357cf1ff19d2
chvmvd <chvmvd@users.noreply.github.com>
2022-09-12 12:38:12 +0900

    add:TSDoc

--- src/components/ReadJSONFile.tsx
+++ src/components/ReadJSONFile.tsx
@@ -1,4 +1,9 @@
-export default function ReadJSONFile(props: { f: (x: object) => void }) {
+/**
+ * ユーザーから受け取った JSON ファイルを引数で指定した関数で処理します。
+ * @param f JSON ファイルを処理する関数
+ * @returns
+ */
+export default function ReadJSONFile({ f }: { f: (x: object) => void }) {
   return (
     <input
       type="file"
@@ -9,7 +14,7 @@ export default function ReadJSONFile(props: { f: (x: object) => void }) {
           const file = e.target.files[0];
           reader.readAsText(file, "utf-8");
           reader.onload = () => {
-            props.f(JSON.parse(reader.result as string));
+            f(JSON.parse(reader.result as string));
           };
         }
       }}
