32d7e26e59af92545276a356041357c5ce6a1d5e
chvmvd <chvmvd@users.noreply.github.com>
2022-09-12 20:13:13 +0900

    スタイルの調整

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,26 @@ import dvorak from "./dvorak.json";
 import jis109 from "./JIS109.json";
 import "./App.css";
 
+/**
+ * `row` 行の `column` 列までの幅の合計を計算します。
+ * @param row `row`
+ * @param column `column`
+ * @param margin `margin`
+ * @returns `width` と `margin` の合計
+ */
+function sumHeight(row: number, column: number, margin: number) {
+  let sum = 1;
+  let j = 0;
+  for (const code of eventCode) {
+    if (jis109[code].row === row && jis109[code].column < column) {
+      sum += jis109[code].width + margin;
+      j++;
+    }
+    if (j >= column) break;
+  }
+  return sum;
+}
+
 export default function App() {
   return (
     <>
@@ -16,7 +36,10 @@ export default function App() {
             style={{
               position: "absolute",
               top: jis109[code].row * 4 + "vw",
-              left: jis109[code].column * 4 + "vw",
+              left:
+                sumHeight(jis109[code].row, jis109[code].column, 0.3 / 3.7) *
+                  3.7 +
+                "vw",
               width: jis109[code].width * 3.7 + "vw",
             }}
             // ref={dom=>{console.log(dom?.textContent)}}
