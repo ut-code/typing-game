063f6ca7826582a6a88d89a055788c369c49cff4
chvmvd <chvmvd@users.noreply.github.com>
2022-09-12 23:20:28 +0900

    selectタグの値を取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import Keyboard from "./components/keyboard";
 
 export default function App() {
+  const [functional, setFunctional] = useState<string>("");
+  const [physical, setPhysical] = useState<string>("");
   return (
     <>
-      <select name="functional-layout" id="functional">
-        <option value="qwerty">QWERTY</option>
+      <select
+        value={functional}
+        onChange={(e) => setFunctional(e.target.value)}
+      >
+        <option value="qwerty" selected>
+          QWERTY
+        </option>
         <option value="dvorak">Dvorak</option>
+      </select>
+      <select value={physical} onChange={(e) => setPhysical(e.target.value)}>
+        <option value="jis109" selected>
+          JIS109
+        </option>
       </select>
       <Keyboard></Keyboard>
     </>
