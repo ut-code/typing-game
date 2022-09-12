8ac20e5f15d6b8db1b33ff7bad02039c83de0972
chvmvd <chvmvd@users.noreply.github.com>
2022-09-12 23:06:37 +0900

    selectタグを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 import Keyboard from "./components/keyboard";
 
 export default function App() {
-  return <Keyboard></Keyboard>;
+  return (
+    <>
+      <select name="functional-layout" id="functional">
+        <option value="qwerty">QWERTY</option>
+        <option value="dvorak">Dvorak</option>
+      </select>
+      <Keyboard></Keyboard>
+    </>
+  );
 }
