3565695698debab2de1a65d5d305869463f55ad8
chvmvd <chvmvd@users.noreply.github.com>
2022-09-13 12:23:06 +0900

    途中

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 // import {useKey} from "react-use";
 import Keyboard from "./components/keyboard";
 import eventCode from "./components/data/eventCode.json";
+import { render } from "react-dom";
 
 export default function App() {
   const [functional, setFunctional] = useState<string>("qwerty");
@@ -9,14 +10,33 @@ export default function App() {
   const [keyColors, setKeyColors] = useState<string[]>(
     eventCode.map((code) => "rgba(0,0,0,0)")
   );
-  // useKey('KeyA',()=>{setKeyColors(eventCode.map((code,i)=>(code==='KeyA'?"lightgreen":keyColors[i])))});
-  // useEffect(()=>{
-  //   window.addEventListener("keydown",(e)=>{
-  //     setKeyColors(eventCode.map((code,i)=>(code===e.code?"lightgreen":keyColors[i])));
-  //   });
-  // },[]);
+  const [test, setTest] = useState<string[]>([]);
+  // useKey('a',()=>{
+  //   // setKeyColors(eventCode.map((code,i)=>(code==='KeyA'?"lightgreen":keyColors[i])));
+  //   setTest([false,test[1]]);
+  // });
+  // useKey('b',()=>{
+  //   // setKeyColors(eventCode.map((code,i)=>(code==='KeyB'?"lightgreen":keyColors[i])))
+  //   setTest([test[0],false]);
+  // });
+  // console.log(test);
+  const keydowne = useCallback(
+    (e: KeyboardEvent) => {
+      setKeyColors(
+        eventCode.map((code, i) =>
+          code === e.code ? "lightgreen" : keyColors[i]
+        )
+      );
+    },
+    [keyColors]
+  );
+
+  useEffect(() => {
+    document.addEventListener("keydown", keydowne, false);
+  }, [keyColors, keydowne]);
   return (
     <>
+      {/* <div tabIndex={0} onKeyDown={(e)=>{setKeyColors(eventCode.map((code,i)=>(code===e.code?"lightgreen":keyColors[i])))}}>aaaaaaaaaaaaaaaaaaa</div> */}
       <select
         value={functional}
         onChange={(e) => setFunctional(e.target.value)}
