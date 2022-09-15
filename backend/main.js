ba4862cda1824509d03a3e9ed0f4fffa5e182c52
chvmvd <chvmvd@users.noreply.github.com>
2022-09-15 16:03:54 +0900

    main.jsを読みやすくした

--- backend/main.js
+++ backend/main.js
@@ -1,11 +1,10 @@
-// Prisma関係
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
-
 const express = require("express");
 const ejs = require("ejs");
 const fs = require("fs");
 const cors = require("cors"); // https://github.com/ut-code/typescript-react-node-template/blob/master/backend/main.ts を参照
+// Prisma関係
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 const app = express();
 
@@ -14,15 +13,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // データベースからPrismaで問題をとってくる
-async function getQuestions() {
-  const records = await prisma.questions.findMany();
-  return records;
-}
-
-//
 app.post("/questions", async (request, response) => {
   // questionsに問題が配列の形で入っている。
-  const records = await getQuestions();
+  const records = await prisma.questions.findMany();
   const questions = records.map((data) => data.question);
   // JSON形式でscript.jsに送信
   response.json(questions);
