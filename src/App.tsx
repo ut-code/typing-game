1d7715e3347a5827320cb37ccf8474fa8653e16b
chvmvd <chvmvd@users.noreply.github.com>
2022-09-16 22:53:42 +0900

    Bug fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,48 @@
-import React, { useState, useEffect} from "react";
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+/* eslint-disable @typescript-eslint/prefer-ts-expect-error */
+import React, { useState, useEffect } from "react";
 import Keyboard from "./components/keyboard";
 import eventCode from "./components/data/eventCode.json";
 import qwerty from "./components/data/qwerty.json";
 import dvorak from "./components/data/dvorak.json";
 import romantable from "./romantable.json";
 import ReadJSONFile from "./../keyboard-layout-maker/src/components/ReadJSONFile";
-import jis109 from "./components/data/JIS109.json"
-const functionalLayoutType = { qwerty, dvorak,custom:qwerty };
-const physicalLayoutType = { jis109,custom:jis109 };
+import jis109 from "./components/data/JIS109.json";
+const functionalLayoutType = { qwerty, dvorak, custom: qwerty };
+const physicalLayoutType = { jis109, custom: jis109 };
 
-function pressed(keyColors:string[],setKeyColors:(value:string[])=>void,code:string,content:string,setContent:(value:string)=>void,functional:string):void{
+function pressed(
+  keyColors: string[],
+  setKeyColors: (value: string[]) => void,
+  code: string,
+  content: string,
+  setContent: (value: string) => void,
+  functional: string
+): void {
   // @ts-ignore
-  setContent((content: string)=>`${content}${functionalLayoutType[functional][code]}`);
+  setContent(
+    (content: string) => `${content}${functionalLayoutType[functional][code]}`
+  );
   setKeyColors(
-    eventCode.map((tmp, i) =>
-      tmp === code ? "orange" : keyColors[i]
-    )
+    eventCode.map((tmp, i) => (tmp === code ? "orange" : keyColors[i]))
   );
   setTimeout(() => {
     setKeyColors(
-      eventCode.map((tmp, i) =>
-        tmp === code ? "rgba(0,0,0,0)" : keyColors[i]
-      )
+      eventCode.map((tmp, i) => (tmp === code ? "rgba(0,0,0,0)" : keyColors[i]))
     );
   }, 100);
 }
 
-function toJapanese(content:string):string{
-  console.log(content)
-  let ans="";
-  let tmp="";
-  for(let i=0;i<content.length;i++){
-    tmp=tmp+content[i].toLowerCase();
-    const hoge=romantable.findIndex((element)=>element[0]===tmp);
-    if(hoge!==-1){
-      ans+=romantable[hoge][1];
-      tmp="";
+function toJapanese(content: string): string {
+  console.log(content);
+  let ans = "";
+  let tmp = "";
+  for (let i = 0; i < content.length; i++) {
+    tmp = tmp + content[i].toLowerCase();
+    const hoge = romantable.findIndex((element) => element[0] === tmp);
+    if (hoge !== -1) {
+      ans += romantable[hoge][1];
+      tmp = "";
     }
   }
   return ans;
@@ -47,26 +54,36 @@ export default function App(): JSX.Element {
   const [keyColors, setKeyColors] = useState<string[]>(
     eventCode.map((code) => "rgba(0,0,0,0)")
   );
-  const [content,setContent]=useState<string>("");
+  const [content, setContent] = useState<string>("");
   useEffect(() => {
-    function tmp(e:KeyboardEvent):void{
-      pressed(keyColors,setKeyColors,e.code,content,setContent,functional);
-    }    
-    window.addEventListener("keydown", tmp);
-    return ()=>{
-      window.removeEventListener("keydown",tmp);
+    function tmp(e: KeyboardEvent): void {
+      pressed(keyColors, setKeyColors, e.code, content, setContent, functional);
     }
-  },[functional]);
+    window.addEventListener("keydown", tmp);
+    return () => {
+      window.removeEventListener("keydown", tmp);
+    };
+  }, [functional]);
   return (
     <>
-    <div>{content}</div>
-    <div>{toJapanese(content)}</div>
-    <span>論理配列</span>
-    {/* @ts-ignore */}
-    <ReadJSONFile f={(x)=>{functionalLayoutType.custom=x}}></ReadJSONFile><br />
-    <span>物理配列</span>
-    {/* @ts-ignore */}
-    <ReadJSONFile f={(x)=>{physicalLayoutType.custom=x}}></ReadJSONFile><br />
+      <div>{content}</div>
+      <div>{toJapanese(content)}</div>
+      <span>論理配列</span>
+      {/* @ts-ignore */}
+      <ReadJSONFile
+        f={(x) => {
+          functionalLayoutType.custom = x;
+        }}
+      ></ReadJSONFile>
+      <br />
+      <span>物理配列</span>
+      {/* @ts-ignore */}
+      <ReadJSONFile
+        f={(x) => {
+          physicalLayoutType.custom = x;
+        }}
+      ></ReadJSONFile>
+      <br />
       <select
         value={functional}
         onChange={(e) => setFunctional(e.target.value)}
--- src/components/keyboard.tsx
+++ src/components/keyboard.tsx
@@ -1,4 +1,6 @@
-// import React from "react"; // ,{useRef}
+/* eslint-disable @typescript-eslint/restrict-plus-operands */
+/* eslint-disable @typescript-eslint/prefer-ts-expect-error */
+import React from "react"; // ,{useRef}
 import eventCode from "./data/eventCode.json";
 import qwerty from "./data/qwerty.json";
 import dvorak from "./data/dvorak.json";
@@ -7,9 +9,9 @@ import "./keyboard.css";
 import useWindowDimensions from "./useWindowDimensions";
 
 let functionalLayout = "qwerty";
-const functionalLayoutType = { qwerty, dvorak,custom:qwerty };
+const functionalLayoutType = { qwerty, dvorak, custom: qwerty };
 let physicalLayout = "jis109";
-const physicalLayoutType = { jis109,custom:jis109 };
+const physicalLayoutType = { jis109, custom: jis109 };
 
 /**
  * `row` 行の `column` 列までの幅の合計を計算します。
@@ -17,18 +19,21 @@ const physicalLayoutType = { jis109,custom:jis109 };
  * @param column `column`
  * @returns `width` と `jis109.eventCode.marginColumn` の合計
  */
-function sumWidth(row: number, column: number):number {
+function sumWidth(row: number, column: number): number {
   // @ts-ignore
   let sum = physicalLayoutType[physicalLayout].marginColumn;
   let j = 0;
   for (const code of eventCode) {
-    if (// @ts-ignore
+    if (
+      // @ts-ignore
       physicalLayoutType[physicalLayout].eventCode[code].row === row &&
       // @ts-ignore
       physicalLayoutType[physicalLayout].eventCode[code].column < column
     ) {
       // @ts-ignore
-      sum += physicalLayoutType[physicalLayout].eventCode[code].width + physicalLayoutType[physicalLayout].marginColumn;
+      sum +=
+        physicalLayoutType[physicalLayout].eventCode[code].width +
+        physicalLayoutType[physicalLayout].marginColumn;
       j++;
     }
     if (j >= column) break;
@@ -41,21 +46,24 @@ function sumWidth(row: number, column: number):number {
  * @param row `row`
  * @returns `row` までの高さの合計
  */
-function sumHeight(row: number):number {
+function sumHeight(row: number): number {
   return (
     // @ts-ignore
-    physicalLayoutType[physicalLayout].marginRow + (physicalLayoutType[physicalLayout].height + physicalLayoutType[physicalLayout].marginRow) * (row - 1)
+    physicalLayoutType[physicalLayout].marginRow +
+    (physicalLayoutType[physicalLayout].height +
+      physicalLayoutType[physicalLayout].marginRow) *
+      (row - 1)
   );
 }
 
-Keyboard.defaultProps={
-  keyColors:"",
-  setKeyColors:(value:string)=>{},
-  pressed:()=>{},
-  content:"",
-  setContent:(value:string)=>{},
-  keyLayout:qwerty
-}
+Keyboard.defaultProps = {
+  keyColors: "",
+  setKeyColors: (value: string) => {},
+  pressed: () => {},
+  content: "",
+  setContent: (value: string) => {},
+  keyLayout: qwerty,
+};
 
 export default function Keyboard({
   functional,
@@ -66,27 +74,36 @@ export default function Keyboard({
   content,
   setContent,
   keyLayout,
-  physicalKeyLayout
+  physicalKeyLayout,
 }: {
   functional: string;
   physical: string;
   keyColors?: string[];
   setKeyColors?: (value: string[]) => void;
-  pressed?:(keyColors:string[],setKeyColors:(value:string[])=>void,code:string,content:string,setContent:(value:string)=>void,functional:string)=>void;
-  content?:string;
-  setContent?:(value:string)=>void;
-  keyLayout?:object;
-  physicalKeyLayout?:object;
-}):JSX.Element {
-  const {width,height}=useWindowDimensions();
-  const magnification=5.8*(width<850?1:850/width);
+  pressed?: (
+    keyColors: string[],
+    setKeyColors: (value: string[]) => void,
+    code: string,
+    content: string,
+    setContent: (value: string) => void,
+    functional: string
+  ) => void;
+  content?: string;
+  setContent?: (value: string) => void;
+  keyLayout?: object;
+  physicalKeyLayout?: object;
+}): JSX.Element {
+  const { width } = useWindowDimensions();
+  const magnification = 5.8 * (width < 850 ? 1 : 850 / width);
 
   functionalLayout = functional;
   physicalLayout = physical;
   // @ts-ignore
-  functionalLayoutType.custom=keyLayout!;
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  functionalLayoutType.custom = keyLayout!;
   // @ts-ignore
-  physicalLayoutType.custom=physicalKeyLayout!;
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  physicalLayoutType.custom = physicalKeyLayout!;
   // const fontSize=0.015; // vw/100
   return (
     <>
@@ -97,19 +114,42 @@ export default function Keyboard({
             id={code}
             className="key"
             // @ts-ignore
-            onClick={()=>pressed(keyColors,setKeyColors,code,content,setContent,functional)}
+            onClick={() =>
+              pressed(
+                keyColors,
+                setKeyColors,
+                code,
+                content,
+                setContent,
+                functional
+              )
+            }
             style={{
               position: "absolute",
               // @ts-ignore
               backgroundColor: keyColors[i],
-              top:// @ts-ignore
-                `${sumHeight(physicalLayoutType[physicalLayout].eventCode[code].row) * magnification}vw`,
-              left:// @ts-ignore
-                `${sumWidth(physicalLayoutType[physicalLayout].eventCode[code].row,physicalLayoutType[physicalLayout].eventCode[code].column) * magnification}vw`,
-              width:// @ts-ignore
-              `${physicalLayoutType[physicalLayout].eventCode[code].width * magnification}vw`,
-              height:// @ts-ignore
-                `${physicalLayoutType[physicalLayout].height * magnification}vw`,
+              // @ts-ignore
+              top: `${
+                sumHeight(
+                  physicalLayoutType[physicalLayout].eventCode[code].row
+                ) * magnification
+              }vw`,
+              // @ts-ignore
+              left: `${
+                sumWidth(
+                  physicalLayoutType[physicalLayout].eventCode[code].row,
+                  physicalLayoutType[physicalLayout].eventCode[code].column
+                ) * magnification
+              }vw`,
+              // @ts-ignore
+              width: `${
+                physicalLayoutType[physicalLayout].eventCode[code].width *
+                magnification
+              }vw`,
+              // @ts-ignore
+              height: `${
+                physicalLayoutType[physicalLayout].height * magnification
+              }vw`,
             }}
             // ref={dom=>{
             //   let fontSizePx=window.innerWidth*fontSize;
