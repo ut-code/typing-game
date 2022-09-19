54557588e746869deeb6391c575d7ed410071887
chvmvd <chvmvd@users.noreply.github.com>
2022-09-19 14:26:49 +0900

    HTMLを内部に書き込み

--- frontend/src/pages/Basic/Basic.tsx
+++ frontend/src/pages/Basic/Basic.tsx
@@ -10,36 +10,30 @@ export default function Basic() {
   }, []);
   return (
     <>
-      <div
-        dangerouslySetInnerHTML={{
-          __html: `
-          <!-- ここから、HTMLファイル -->
-        <div id="elements">
-              <p id="mondai">問題</p>
-              <p id="question">スペースキーを押して開始</p>
-              <table id="answer">
-                <th id="header">あなたの答え</th>
-                <td id="your-answer"></td>
-              </table>
-              <table id="result">
-                <tr>
-                  <th>正しいタイプ数：</th>
-                  <td id="correct"></td>
-                </tr>
-                <tr>
-                  <th>ミスタイプ数：</th>
-                  <td id="miss"></td>
-                </tr>
-                <tr>
-                  <th>経過時間：</th>
-                  <td id="time"></td>
-                </tr>
-              </table>
-            </div>
-            <!-- ここまでHTMLファイル -->
-        `,
-        }}
-      ></div>
+      {/* ここからHTMLファイル */}
+      <div id="elements">
+        <p id="mondai">問題</p>
+        <p id="question">スペースキーを押して開始</p>
+        <table id="answer">
+          <th id="header">あなたの答え</th>
+          <td id="your-answer"></td>
+        </table>
+        <table id="result">
+          <tr>
+            <th>正しいタイプ数：</th>
+            <td id="correct"></td>
+          </tr>
+          <tr>
+            <th>ミスタイプ数：</th>
+            <td id="miss"></td>
+          </tr>
+          <tr>
+            <th>経過時間：</th>
+            <td id="time"></td>
+          </tr>
+        </table>
+      </div>
+      {/* ここまでHTMLファイル */}
       キーボード配列を自分で作りたい人は、
       <a href="https://keyboard-layout-maker.onrender.com/">このリンク</a>
       に飛んでください。
