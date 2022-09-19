36937e20a09732fb2cf685c8a9dd0f9189f0f67c
chvmvd <chvmvd@users.noreply.github.com>
2022-09-19 15:16:52 +0900

    微修正

--- frontend/src/pages/Home/Basic/Basic.tsx
+++ frontend/src/pages/Home/Basic/Basic.tsx
@@ -34,9 +34,6 @@ export default function Basic() {
         </table>
       </div>
       {/* ここまでHTMLファイル */}
-      キーボード配列を自分で作りたい人は、
-      <a href="https://keyboard-layout-maker.onrender.com/">このリンク</a>
-      に飛んでください。
       <Keyboard />
     </>
   );
