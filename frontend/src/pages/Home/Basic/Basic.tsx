a87e333e968198d662b66522021c17d213eaed11
chvmvd <chvmvd@users.noreply.github.com>
2022-09-19 16:31:51 +0900

    スペースキー問題を解決

--- frontend/src/pages/Home/Basic/Basic.tsx
+++ frontend/src/pages/Home/Basic/Basic.tsx
@@ -12,35 +12,48 @@ export default function Basic() {
     <>
       {/* ここからHTMLファイル */}
       <div id="elements">
-        <p id="mondai">問題</p>
-        <p id="question">スペースキーを押して開始</p>
-        <table id="answer">
-          <tbody>
-            <tr>
-              <th id="header">あなたの答え</th>
-              <td id="your-answer"></td>
-            </tr>
-          </tbody>
-        </table>
-        <table id="result">
-          <tbody>
-            <tr>
-              <th>正しいタイプ数：</th>
-              <td id="correct"></td>
-            </tr>
-            <tr>
-              <th>ミスタイプ数：</th>
-              <td id="miss"></td>
-            </tr>
-            <tr>
-              <th>経過時間：</th>
-              <td id="time"></td>
-            </tr>
-          </tbody>
-        </table>
+        <div>
+          <p id="mondai">問題</p>
+          <p id="question">スペースキーを押して開始</p>
+          <table id="answer">
+            <tbody>
+              <tr>
+                <th id="header">あなたの答え</th>
+                <td id="your-answer"></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div>
+          <table id="result">
+            <p>結果</p>
+            <tbody>
+              <tr>
+                <th>正しいタイプ数：</th>
+                <td id="correct"></td>
+              </tr>
+              <tr>
+                <th>ミスタイプ数：</th>
+                <td id="miss"></td>
+              </tr>
+              <tr>
+                <th>経過時間：</th>
+                <td id="time"></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
       {/* ここまでHTMLファイル */}
-      <Keyboard />
+      <Keyboard
+        element={
+          <>
+            キーボード配列を自分で作りたい人は、
+            <a href="https://keyboard-layout-maker.onrender.com/">このリンク</a>
+            に飛んでください。
+          </>
+        }
+      />
     </>
   );
 }
