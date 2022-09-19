e56aaf80e778f79de682d596b9b7581878dfff3a
chvmvd <chvmvd@users.noreply.github.com>
2022-09-19 09:48:55 +0900

    もとに戻した

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,4 @@
 import Keyboard from "./../../keyboard/src/App";
 export default function App() {
-  return (
-    <>
-      キーボード配列を自分で作りたい人は、
-      <a href="https://keyboard-layout-maker.onrender.com/">このリンク</a>
-      に飛んでください。
-      <Keyboard />
-    </>
-  );
+  return <Keyboard />;
 }
