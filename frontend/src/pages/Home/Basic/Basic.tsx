9e3bf43bd5622dd24425ce551da380305d3e318c
baar-vendel <fhinnoigsr@gmail.com>
2022-09-23 18:57:19 +0900

    react bootstrapを使ってHOMEのビジュアルを改善

--- frontend/src/pages/Home/Basic/Basic.tsx
+++ frontend/src/pages/Home/Basic/Basic.tsx
@@ -14,47 +14,49 @@ export default function Basic() {
   return (
     <>
       {/* ここからHTMLファイル */}
-      <div id="elements">
-        <div>
-          <p id="mondai">問題</p>
-          <p id="question">スペースキーを押して開始</p>
-          <table id="answer">
-            <tbody>
-              <tr>
-                <th id="header">あなたの答え</th>
-                <td id="your-answer"></td>
-              </tr>
-            </tbody>
-          </table>
+      <body>
+        <div id="elements">
+          <div>
+            <p id="mondai">問題</p>
+            <p id="question">スペースキーを押して開始</p>
+            <table id="answer">
+              <tbody>
+                <tr>
+                  <th id="header">あなたの答え</th>
+                  <td id="your-answer"></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div>
+            <table id="result">
+              <thead>
+                <tr>
+                  <th>結果</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th>正しいタイプ数：</th>
+                  <td id="correct"></td>
+                </tr>
+                <tr>
+                  <th>ミスタイプ数：</th>
+                  <td id="miss"></td>
+                </tr>
+                <tr>
+                  <th>経過時間：</th>
+                  <td id="time"></td>
+                </tr>
+                <tr>
+                  <th>残り時間：</th>
+                  <td id="timeLeft"></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
-        <div>
-          <table id="result">
-            <thead>
-              <tr>
-                <th>結果</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th>正しいタイプ数：</th>
-                <td id="correct"></td>
-              </tr>
-              <tr>
-                <th>ミスタイプ数：</th>
-                <td id="miss"></td>
-              </tr>
-              <tr>
-                <th>経過時間：</th>
-                <td id="time"></td>
-              </tr>
-              <tr>
-                <th>残り時間：</th>
-                <td id="timeLeft"></td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
+      </body>
       {/* ここまでHTMLファイル */}
       {/* 下のdivの中にReactがキーボードの入力結果をいい感じにして、出力している。これを、読み取って使えば良い。 */}
       <div id="content"></div>
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -3,6 +3,9 @@ import { useEffect } from "react";
 import script from "./script.js";
 import "./style.css";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Form } from "react-bootstrap";
+
 export default function Home() {
   useEffect(() => {
     script();
@@ -10,11 +13,40 @@ export default function Home() {
   return (
     <>
       <header>
-        <b>タイピングゲーム ホーム画面</b>
+        <b>ut.code(); タイピングゲーム</b>
       </header>
-      ユーザーネーム: <input name="username" placeholder="Guest" />
-      問題番号: <input name="questionNumber" placeholder="0" />
-      <button id="play-button">プレイする</button>
+      <body>
+        {/* ユーザーネーム: <input name="username" placeholder="Guest" />
+        問題番号: <input name="questionNumber" placeholder="0" /> */}
+
+        <div id="userinput">
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>ユーザーネーム</Form.Label>
+              <Form.Control placeholder="Guest" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>問題番号</Form.Label>
+              <Form.Select>
+                <option>0</option>
+                <option>11</option>
+              </Form.Select>
+              <Form.Text className="text-muted">
+                0: テスト用 11: ゲティスバーグ演説
+              </Form.Text>
+            </Form.Group>
+
+            <Button variant="secondary" id="play-button">
+              プレイする
+            </Button>
+          </Form>
+        </div>
+      </body>
+      <footer>
+        <hr></hr>
+        <p>© Copyright 2022 ut.code();</p>
+      </footer>
     </>
   );
 }
