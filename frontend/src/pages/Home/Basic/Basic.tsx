90154fbdeddb285b90940cd3e6c3b4bd56a90cfe
baar-vendel <fhinnoigsr@gmail.com>
2022-09-24 10:59:44 +0900

    /basicを多少マシな状態にした

--- frontend/src/pages/Home/Basic/Basic.tsx
+++ frontend/src/pages/Home/Basic/Basic.tsx
@@ -5,7 +5,7 @@ import "./style.css";
 import script from "./script";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button, Form } from "react-bootstrap";
+import { Button, ProgressBar } from "react-bootstrap";
 
 export default function Basic() {
   const [content, setContent] = useState<string>("a");
@@ -20,45 +20,44 @@ export default function Basic() {
       <body>
         <div id="elements">
           <div>
-            <p id="mondai">問題</p>
-            <p id="question">スペースキーを押して開始</p>
-            <table id="answer">
+            {/* <p id="mondai">問題</p> */}
+            <p id="question">[Space]を押して開始</p>
+            <p id="your-answer"></p>
+            {/* <table id="answer">
               <tbody>
                 <tr>
                   <th id="header">あなたの答え</th>
                   <td id="your-answer"></td>
                 </tr>
               </tbody>
-            </table>
-          </div>
-          <div>
-            <table id="result">
-              <thead>
-                <tr>
-                  <th>結果</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <th>正しいタイプ数：</th>
-                  <td id="correct"></td>
-                </tr>
-                <tr>
-                  <th>ミスタイプ数：</th>
-                  <td id="miss"></td>
-                </tr>
-                <tr>
-                  <th>経過時間：</th>
-                  <td id="time"></td>
-                </tr>
-                <tr>
-                  <th>残り時間：</th>
-                  <td id="timeLeft"></td>
-                </tr>
-              </tbody>
-            </table>
+            </table> */}
           </div>
         </div>
+        <div>
+          <table id="result">
+            <tbody>
+              <tr>
+                <th>正しいタイプ数：</th>
+                <td id="correct"></td>
+              </tr>
+              <tr>
+                <th>ミスタイプ数：</th>
+                <td id="miss"></td>
+              </tr>
+              <tr>
+                <th>経過時間：</th>
+                <td id="time"></td>
+              </tr>
+              <tr>
+                <th>残り時間：</th>
+                <td id="timeLeft"></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <Button href="/" variant="secondary">
+          Back
+        </Button>
       </body>
       {/* ここまでHTMLファイル */}
       {/* 下のdivの中にReactがキーボードの入力結果をいい感じにして、出力している。これを、読み取って使えば良い。 */}
