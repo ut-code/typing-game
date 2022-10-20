ec5fce1f196e422f166e7d9aaee32ff6103ecfa1
baar-vendel <fhinnoigsr@gmail.com>
2022-10-20 20:55:42 +0900

    仮ロゴを作成、faviconはなぜか設定できてない

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Image } from "react-bootstrap";
+
+import logo from "./logo.png";
+
 export default function Header() {
   return (
     <header>
-      <b>ut.code(); タイピングゲーム</b>
+      {/* <b>ut.code(); タイピングゲーム</b> */}
+      <Image src={logo} alt={"タイピングゲーム by ut.code();"} />
     </header>
   );
 }
