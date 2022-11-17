d1163f0bc5799225acd78237efa562bcd0c99777
chvmvd <chvmvd@users.noreply.github.com>
2022-11-17 20:39:17 +0900

    jis_qwertyの設定を修正

--- keyboard/src/components/data/keyboardSettings.tsx
+++ keyboard/src/components/data/keyboardSettings.tsx
@@ -6,12 +6,12 @@ import us_default from "./us_default.json";
 import jis109 from "./JIS109.json";
 
 export const layoutType = {
-  us_qwerty: { name:"US QWERTY", id:"us_qwerty", functionalLayoutType: "us_qwerty", physicalLayoutType: "us_default" },
-  jis_qwerty: {name:"JIS109 QWERTY", id:"jis_qwerty", functionalLayoutType: "us_qwerty", physicalLayoutType: "jis109" },
-  us_dvorak: { name:"Dvorak", id:"us_dvorak", functionalLayoutType: "us_dvorak", physicalLayoutType: "us_default" },
-  azerty: { name:"AZERTY", id:"azerty", functionalLayoutType: "azerty", physicalLayoutType: "us_default" },
-  qwertz: { name:"QWERTZ", id:"qwertz", functionalLayoutType: "qwertz", physicalLayoutType: "us_default" },
-  custom: { name:"Custom", id:"custom", functionalLayoutType: "custom", physicalLayoutType: "custom" },
+  us_qwerty: { name: "US QWERTY", id: "us_qwerty", functionalLayoutType: "us_qwerty", physicalLayoutType: "us_default" },
+  jis_qwerty: { name: "JIS109 QWERTY", id: "jis_qwerty", functionalLayoutType: "jis_qwerty", physicalLayoutType: "jis109" },
+  us_dvorak: { name: "Dvorak", id: "us_dvorak", functionalLayoutType: "us_dvorak", physicalLayoutType: "us_default" },
+  azerty: { name: "AZERTY", id: "azerty", functionalLayoutType: "azerty", physicalLayoutType: "us_default" },
+  qwertz: { name: "QWERTZ", id: "qwertz", functionalLayoutType: "qwertz", physicalLayoutType: "us_default" },
+  custom: { name: "Custom", id: "custom", functionalLayoutType: "custom", physicalLayoutType: "custom" },
 };
 
 export const functionalLayoutType = {
@@ -27,7 +27,7 @@ export const physicalLayoutType = {
   custom: { name: "Custom", id: "custom", content: us_default },
 };
 
-export const defaultlayoutType="us_qwerty";
+export const defaultlayoutType = "us_qwerty";
 
 export const defaultFunctionalLayoutType = "us_qwerty";
 
