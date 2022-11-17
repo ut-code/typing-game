e6266d82115e3147d612da28fdd39587e9dbf04b
chvmvd <chvmvd@users.noreply.github.com>
2022-11-18 00:55:25 +0900

    update dvorak_jis

--- keyboard/src/components/data/keyboardSettings.tsx
+++ keyboard/src/components/data/keyboardSettings.tsx
@@ -6,6 +6,7 @@ import azerty from "./azerty.json";
 import qwertz from "./qwertz.json";
 import us_default from "./us_default.json";
 import jis109 from "./jis109.json";
+import dvorak_jis from "./dvorak_jis.json";
 
 export const functionalLayoutType = {
   jis_qwerty: { content: jis_qwerty },
@@ -19,6 +20,7 @@ export const functionalLayoutType = {
 export const physicalLayoutType = {
   us_default: { content: us_default },
   jis109: { content: jis109 },
+  dvorak_jis: { content: dvorak_jis },
   custom: { content: jis109 },
 };
 
@@ -26,7 +28,7 @@ export const layoutType = {
   jis_qwerty: { name: "JIS QWERTY", id: "jis_qwerty", functionalLayoutType: "jis_qwerty", physicalLayoutType: "jis109" },
   mac_jis_qwerty: { name: "Mac JIS QWERTY", id: "mac_jis_qwerty", functionalLayoutType: "mac_jis_qwerty", physicalLayoutType: "jis109" },
   mac_us_qwerty: { name: "Mac US QWERTY", id: "mac_us_qwerty", functionalLayoutType: "mac_us_qwerty", physicalLayoutType: "us_default" },
-  dvorak: { name: "Dvorak", id: "dvorak", functionalLayoutType: "dvorak", physicalLayoutType: "jis109" },
+  dvorak: { name: "Dvorak", id: "dvorak", functionalLayoutType: "dvorak", physicalLayoutType: "dvorak_jis" },
   azerty: { name: "AZERTY", id: "azerty", functionalLayoutType: "azerty", physicalLayoutType: "jis109" },
   qwertz: { name: "QWERTZ", id: "qwertz", functionalLayoutType: "qwertz", physicalLayoutType: "jis109" },
   custom: { name: "Custom", id: "custom", functionalLayoutType: "custom", physicalLayoutType: "custom" },
