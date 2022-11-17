a648f5f19d2e62b528083f89ed1734831c1b09b7
chvmvd <chvmvd@users.noreply.github.com>
2022-11-17 23:44:34 +0900

    KeyboardSettingsを更新

--- keyboard/src/components/data/keyboardSettings.tsx
+++ keyboard/src/components/data/keyboardSettings.tsx
@@ -1,36 +1,36 @@
 import jis_qwerty from "./jis_qwerty.json";
 import us_qwerty from "./us_qwerty.json";
-import us_dvorak from "./us_dvorak.json";
+import us_dvorak from "./dvorak.json";
 import azerty from "./azerty.json";
 import qwertz from "./qwertz.json";
 import us_default from "./us_default.json";
-import jis109 from "./JIS109.json";
-
-export const layoutType = {
-  us_qwerty: { name: "US QWERTY", id: "us_qwerty", functionalLayoutType: "us_qwerty", physicalLayoutType: "us_default" },
-  jis_qwerty: { name: "JIS109 QWERTY", id: "jis_qwerty", functionalLayoutType: "jis_qwerty", physicalLayoutType: "jis109" },
-  us_dvorak: { name: "Dvorak", id: "us_dvorak", functionalLayoutType: "us_dvorak", physicalLayoutType: "us_default" },
-  azerty: { name: "AZERTY", id: "azerty", functionalLayoutType: "azerty", physicalLayoutType: "us_default" },
-  qwertz: { name: "QWERTZ", id: "qwertz", functionalLayoutType: "qwertz", physicalLayoutType: "us_default" },
-  custom: { name: "Custom", id: "custom", functionalLayoutType: "custom", physicalLayoutType: "custom" },
-};
+import jis109 from "./jis109.json";
 
 export const functionalLayoutType = {
-  jis_qwerty: { name: "JIS_QWERTY", id: "jis_qwerty", content: jis_qwerty },
-  us_qwerty: { name: "US_QWERTY", id: "us_qwerty", content: us_qwerty },
-  us_dvorak: { name: "US_Dvorak", id: "us_dvorak", content: us_dvorak },
-  azerty: { name: "AZERTY", id: "azerty", content: azerty },
-  qwertz: { name: "QWERTZ", id: "qwertz", content: qwertz },
-  custom: { name: "Custom", id: "custom", content: us_qwerty },
+  jis_qwerty: { content: jis_qwerty },
+  us_qwerty: { content: us_qwerty },
+  us_dvorak: { content: us_dvorak },
+  azerty: { content: azerty },
+  qwertz: { content: qwertz },
+  custom: { content: jis_qwerty },
 };
 export const physicalLayoutType = {
-  us_default: { name: "US_Default", id: "us_default", content: us_default },
-  jis109: { name: "JIS109", id: "jis109", content: jis109 },
-  custom: { name: "Custom", id: "custom", content: us_default },
+  us_default: { content: us_default },
+  jis109: { content: jis109 },
+  custom: { content: jis109 },
 };
 
-export const defaultlayoutType = "jis_qwerty";
+export const layoutType = {
+  jis_qwerty: { name: "JIS QWERTY", id: "jis_qwerty", functionalLayoutType: "jis_qwerty", physicalLayoutType: "jis109" },
+  us_qwerty: { name: "US QWERTY", id: "us_qwerty", functionalLayoutType: "us_qwerty", physicalLayoutType: "us_default" },
+  dvorak: { name: "Dvorak", id: "dvorak", functionalLayoutType: "dvorak", physicalLayoutType: "jis109" },
+  azerty: { name: "AZERTY", id: "azerty", functionalLayoutType: "azerty", physicalLayoutType: "jis109" },
+  qwertz: { name: "QWERTZ", id: "qwertz", functionalLayoutType: "qwertz", physicalLayoutType: "jis109" },
+  custom: { name: "Custom", id: "custom", functionalLayoutType: "custom", physicalLayoutType: "custom" },
+};
 
 export const defaultFunctionalLayoutType = "jis_qwerty";
 
 export const defaultPhysicalLayoutType = "jis109";
+
+export const defaultlayoutType = "jis_qwerty";
