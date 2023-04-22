ab158efbe6d93922ef3744bd28bcb9eaa752441e
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-04-22 11:27:13 +0900

    Rename Result.tsx to index.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import Basic from "./pages/Basic"
-import Result from "./pages/Result/Result"
+import Result from "./pages/Result"
 export default function App() {
   return (
     <>
