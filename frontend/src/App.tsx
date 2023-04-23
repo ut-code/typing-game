c435a42da04e33fd0c90ae12072ad4a11cadcd21
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-04-23 19:06:18 +0900

    Move home

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom"
 import Home from "./pages"
 import Basic from "./pages/Basic"
 import Result from "./pages/Result"
+import KeyboardLayoutCreatorHome from "./pages/KeyboardLayoutCreator/Home"
 export default function App() {
   return (
     <>
@@ -10,6 +11,7 @@ export default function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/basic" element={<Basic />}></Route>
         <Route path="/result" element={<Result />}></Route>
+        <Route path="/keyboard-layout-creator" element={<KeyboardLayoutCreatorHome />}></Route>
       </Routes>
     </>
   )
--- frontend/src/pages/KeyboardLayoutCreator/Home.tsx
+++ frontend/src/pages/KeyboardLayoutCreator/Home.tsx
@@ -1,19 +1,15 @@
-import React, { Link } from "react-router-dom";
-import howToUse from "./how_to_use.mp4";
-import "./Home.css";
+import React, { Link } from "react-router-dom"
+import howToUse from "./how_to_use.mp4"
+import "./Home.css"
 export default function Home(): JSX.Element {
   return (
     <>
       <main>
         <h2>Keyboard Layout Creator</h2>
-        <p>
-          駒場祭のタイピングゲームのキーボード配列を作成するためのプログラムです。
-        </p>
+        <p>駒場祭のタイピングゲームのキーボード配列を作成するためのプログラムです。</p>
         <p>
           論理配列と物理配列をそれぞれ自分で作成して、
-          <a href="https://typing-game.onrender.com/">
-            駒場祭のタイピングゲーム
-          </a>
+          <a href="https://typing-game.onrender.com/">駒場祭のタイピングゲーム</a>
           でそれを使用することができます。自分にあったキーボード配列を作ってタイピング速度を上げてみてください。
         </p>
         <p>
@@ -22,11 +18,11 @@ export default function Home(): JSX.Element {
 
         <video controls muted src={howToUse} width="700px" />
       </main>
-      <nav>
+      {/* <nav>
         <Link to="/functional">論理配列</Link>
         <br />
         <Link to="/physical">物理配列</Link>
-      </nav>
+      </nav> */}
     </>
-  );
+  )
 }
