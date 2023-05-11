5194b283eb0bd9a0fb43eff5cfe42d7656ff6aec
bvv-1 <fhinnoigsr@gmail.com>
2023-05-11 20:16:30 +0900

    Improve style.css

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -39,57 +39,56 @@ export default function Home() {
   return (
     <>
       <Header />
-      <Stack gap={3}>
-        <div id="userinput">
-          <Form>
-            <Form.Group className="mb-3" controlId="username">
-              <Form.Label>
-                <b>ユーザーネーム（20文字まで）</b>
-              </Form.Label>
-              <Form.Control
-                placeholder="Guest"
-                value={userName}
-                onChange={(e) => {
-                  setUserName(e.target.value)
-                }}
-                maxLength={20}
-              />
-            </Form.Group>
 
-            <Form.Group className="mb-3" controlId="qnumber">
-              <Form.Label>
-                <b>問題番号</b>
-              </Form.Label>
-              <Form.Select
-                value={qnumber}
-                onChange={(e) => {
-                  setQnumber(e.target.value)
-                }}
-              >
-                <option>0: 関東地方</option>
-                <option>1: マックのメニュー</option>
-                <option>2: 日本食</option>
-                <option>3: 惑星名</option>
-                <option>4: 短い英単語集</option>
-                <option>5: 長い英単語集</option>
-                <option>6: アクセス数の多いサイト</option>
-                <option>7: SDGs</option>
-                <option>8: MDGs</option>
-                <option>9: キーボード配列</option>
-                {/* <option>10: htmlを書いてみよう</option> */}
-                <option>11: ゲティスバーグ演説</option>
-                <option>20: ロンドン橋落ちた</option>
-                <option>21: ハンプティダンプティ</option>
-                <option>22: きらきら星</option>
-                <option>23: Hey Diddle Diddle</option>
-              </Form.Select>
-            </Form.Group>
+      <div className="elements">
+        <Form>
+          <Form.Group className="mb-3" controlId="username">
+            <Form.Label>
+              <b>ユーザーネーム（20文字まで）</b>
+            </Form.Label>
+            <Form.Control
+              placeholder="Guest"
+              value={userName}
+              onChange={(e) => {
+                setUserName(e.target.value)
+              }}
+              maxLength={20}
+            />
+          </Form.Group>
 
-            <Button variant="secondary" id="play-button" onClick={postStorage}>
-              Play
-            </Button>
-          </Form>
-        </div>
+          <Form.Group className="mb-3" controlId="qnumber">
+            <Form.Label>
+              <b>問題番号</b>
+            </Form.Label>
+            <Form.Select
+              value={qnumber}
+              onChange={(e) => {
+                setQnumber(e.target.value)
+              }}
+            >
+              <option>0: 関東地方</option>
+              <option>1: マックのメニュー</option>
+              <option>2: 日本食</option>
+              <option>3: 惑星名</option>
+              <option>4: 短い英単語集</option>
+              <option>5: 長い英単語集</option>
+              <option>6: アクセス数の多いサイト</option>
+              <option>7: SDGs</option>
+              <option>8: MDGs</option>
+              <option>9: キーボード配列</option>
+              {/* <option>10: htmlを書いてみよう</option> */}
+              <option>11: ゲティスバーグ演説</option>
+              <option>20: ロンドン橋落ちた</option>
+              <option>21: ハンプティダンプティ</option>
+              <option>22: きらきら星</option>
+              <option>23: Hey Diddle Diddle</option>
+            </Form.Select>
+          </Form.Group>
+
+          <Button variant="secondary" id="play-button" onClick={postStorage}>
+            Play
+          </Button>
+        </Form>
 
         <Accordion defaultActiveKey="0" alwaysOpen id="how-to-play">
           <Accordion.Item eventKey="0">
@@ -103,7 +102,8 @@ export default function Home() {
             </Accordion.Body>
           </Accordion.Item>
         </Accordion>
-      </Stack>
+      </div>
+
       <Footer />
     </>
   )
