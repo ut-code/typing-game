56fff0e7e83f2cfc5bc501a2c17efb2620cd5588
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-06-18 19:29:28 +0900

    Add ranking data to seed

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -119,11 +119,70 @@ const questionSets: QuestionSet[] = [
   },
 ]
 
+type TypingResult = { questionNumber: number; userName: string; score: number }
+
+const typingResults: TypingResult[] = [
+  { questionNumber: 0, userName: "Alice", score: 200000 },
+  { questionNumber: 0, userName: "Bob", score: 150000 },
+  { questionNumber: 0, userName: "Developer", score: 1000000 },
+  { questionNumber: 1, userName: "田沼意次", score: 150000 },
+  { questionNumber: 1, userName: "松平定信", score: 150000 },
+  { questionNumber: 1, userName: "水野忠邦", score: 150000 },
+  { questionNumber: 2, userName: "織田信長", score: 200000 },
+  { questionNumber: 2, userName: "豊臣秀吉", score: 100000 },
+  { questionNumber: 2, userName: "徳川家康", score: 100000 },
+  { questionNumber: 2, userName: "今川義元", score: 90000 },
+  { questionNumber: 2, userName: "明智光秀", score: 50000 },
+  { questionNumber: 2, userName: "伊達政宗", score: 70000 },
+  { questionNumber: 2, userName: "武田信玄", score: 80000 },
+  { questionNumber: 2, userName: "上杉謙信", score: 80000 },
+  { questionNumber: 2, userName: "浅井長政", score: 30000 },
+  { questionNumber: 2, userName: "朝倉義景", score: 30000 },
+  { questionNumber: 2, userName: "毛利元就", score: 50000 },
+  { questionNumber: 2, userName: "島津義久", score: 50000 },
+]
+
 async function main() {
+  // Seed question sets
   for (const questionSet of questionSets) {
     for (const question of questionSet.questions) {
       await prisma.questions.create({ data: { qnumber: questionSet.questionSetNumber, question: question } })
     }
+  }
+
+  // Seed whole ranking
+  for (const typingResult of typingResults) {
+    await prisma.ranking.create({
+      data: {
+        problem: typingResult.questionNumber,
+        username: typingResult.userName,
+        score: typingResult.score,
+      },
+    })
+  }
+
+  // Seed ranking for KF73
+  for (let i = 3; i < 6; i += 1) {
+    const typingResult = typingResults[i]
+    await prisma.ranking_kf73.create({
+      data: {
+        problem: typingResult.questionNumber,
+        username: typingResult.userName,
+        score: typingResult.score,
+      },
+    })
+  }
+
+  // Seed ranking for MF96
+  for (let i = 6; i < 16; i += 1) {
+    const typingResult = typingResults[i]
+    await prisma.ranking_mf96.create({
+      data: {
+        problem: typingResult.questionNumber,
+        username: typingResult.userName,
+        score: typingResult.score,
+      },
+    })
   }
 }
 
