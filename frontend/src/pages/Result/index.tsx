2de8aa1c667d622fee62c3c6fe77774ef1aa20ec
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-10-06 23:59:40 +0900

    Remove unnecessary `div` element

--- frontend/src/pages/Result/index.tsx
+++ frontend/src/pages/Result/index.tsx
@@ -1,5 +1,3 @@
-// CSS関連
-import styles from "./styles.module.css";
 import { Stack, Accordion } from "react-bootstrap";
 
 // コンポーネント
@@ -20,40 +18,38 @@ export default function Result() {
     <>
       <Header />
       <Stack gap={3}>
-        <div className={styles.resultElements}>
-          <BackButton />
-          <Stack direction="horizontal" gap={3}>
-            <PerformanceSummary typingSessionId={typingSessionId} />
-            <Ranking />
-          </Stack>
-          <Accordion defaultActiveKey="1">
-            <Accordion.Item eventKey="0">
-              <Accordion.Header>総合ランクの基準</Accordion.Header>
-              <Accordion.Body>
-                <div>
-                  SS: 平均タイプ数 5.00 回/秒以上 かつ ミスタイプ率 0% かつ 完答
-                </div>
-                <div>
-                  S: 平均タイプ数 5.00 回/秒以上 かつ ミスタイプ率 10%未満 かつ
-                  完答
-                </div>
-                <div>
-                  A: 平均タイプ数 4.00 回/秒以上 かつ ミスタイプ率 20%未満
-                </div>
-                <div>
-                  B: 平均タイプ数 3.00 回/秒以上 かつ ミスタイプ率 20%未満
-                </div>
-                <div>
-                  C: 平均タイプ数 2.00 回/秒以上 かつ ミスタイプ率 30%未満
-                </div>
-                <div>
-                  D: 平均タイプ数 2.00 回/秒未満 かつ ミスタイプ率 50%未満
-                </div>
-                <div>E: ミスタイプ率 50%以上</div>
-              </Accordion.Body>
-            </Accordion.Item>
-          </Accordion>
-        </div>
+        <BackButton />
+        <Stack direction="horizontal" gap={3}>
+          <PerformanceSummary typingSessionId={typingSessionId} />
+          <Ranking />
+        </Stack>
+        <Accordion defaultActiveKey="1">
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>総合ランクの基準</Accordion.Header>
+            <Accordion.Body>
+              <div>
+                SS: 平均タイプ数 5.00 回/秒以上 かつ ミスタイプ率 0% かつ 完答
+              </div>
+              <div>
+                S: 平均タイプ数 5.00 回/秒以上 かつ ミスタイプ率 10%未満 かつ
+                完答
+              </div>
+              <div>
+                A: 平均タイプ数 4.00 回/秒以上 かつ ミスタイプ率 20%未満
+              </div>
+              <div>
+                B: 平均タイプ数 3.00 回/秒以上 かつ ミスタイプ率 20%未満
+              </div>
+              <div>
+                C: 平均タイプ数 2.00 回/秒以上 かつ ミスタイプ率 30%未満
+              </div>
+              <div>
+                D: 平均タイプ数 2.00 回/秒未満 かつ ミスタイプ率 50%未満
+              </div>
+              <div>E: ミスタイプ率 50%以上</div>
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
       </Stack>
       <Footer />
     </>
