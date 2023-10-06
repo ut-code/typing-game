68fbe219b31ba66cc1ebc4f2987eae51d6ed1d9a
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-10-07 00:03:11 +0900

    Create `RankingCriteria` component

--- frontend/src/pages/Result/index.tsx
+++ frontend/src/pages/Result/index.tsx
@@ -1,4 +1,4 @@
-import { Stack, Accordion } from "react-bootstrap";
+import { Stack } from "react-bootstrap";
 
 // コンポーネント
 import Header from "../../components/Header";
@@ -7,6 +7,7 @@ import BackButton from "../../components/BackButton";
 import { useParams } from "react-router-dom";
 import Ranking from "./components/Ranking/Ranking";
 import PerformanceSummary from "./components/PerformanceSummary/PerformanceSummary";
+import RankingCriteria from "./components/RankingCriteria";
 
 export default function Result() {
   const { uuid: typingSessionId } = useParams();
@@ -23,33 +24,7 @@ export default function Result() {
           <PerformanceSummary typingSessionId={typingSessionId} />
           <Ranking />
         </Stack>
-        <Accordion defaultActiveKey="1">
-          <Accordion.Item eventKey="0">
-            <Accordion.Header>総合ランクの基準</Accordion.Header>
-            <Accordion.Body>
-              <div>
-                SS: 平均タイプ数 5.00 回/秒以上 かつ ミスタイプ率 0% かつ 完答
-              </div>
-              <div>
-                S: 平均タイプ数 5.00 回/秒以上 かつ ミスタイプ率 10%未満 かつ
-                完答
-              </div>
-              <div>
-                A: 平均タイプ数 4.00 回/秒以上 かつ ミスタイプ率 20%未満
-              </div>
-              <div>
-                B: 平均タイプ数 3.00 回/秒以上 かつ ミスタイプ率 20%未満
-              </div>
-              <div>
-                C: 平均タイプ数 2.00 回/秒以上 かつ ミスタイプ率 30%未満
-              </div>
-              <div>
-                D: 平均タイプ数 2.00 回/秒未満 かつ ミスタイプ率 50%未満
-              </div>
-              <div>E: ミスタイプ率 50%以上</div>
-            </Accordion.Body>
-          </Accordion.Item>
-        </Accordion>
+        <RankingCriteria />
       </Stack>
       <Footer />
     </>
