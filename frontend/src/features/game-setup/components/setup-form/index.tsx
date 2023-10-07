07658d25cbdc254a846a637966557463a402e305
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-10-07 10:00:46 +0900

    Create `SetupForm` component

---
+++ frontend/src/features/game-setup/components/setup-form/index.tsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Button, Form } from "react-bootstrap";
+import typingGameQuestionSets from "@typing-game/question-sets";
+import UserNameInput from "./components/UserNameInput";
+import QuestionSetInput from "./components/QuestionSetInput";
+
+export default function SetupForm(): JSX.Element {
+  const [userName, setUserName] = useState<string>(
+    localStorage.getItem("username") || "",
+  );
+  const [questionSetId, setQuestionSetId] = useState<string>(
+    typingGameQuestionSets[0].id,
+  );
+  // useNavigate を Navigate に変化させる呪文
+  const Navigate = useNavigate();
+
+  // ユーザーの入力情報を受け取る関数
+  const postStorage = () => {
+    localStorage.setItem("username", userName || "Guest");
+    localStorage.setItem("questionSetId", questionSetId);
+    // fetchAPI後に別ページへ遷移
+    Navigate("/basic");
+  };
+
+  // localStorageをリセットする
+  let unmounted = false;
+  useEffect(() => {
+    if (unmounted) return;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    unmounted = true;
+    Object.keys(localStorage).forEach((key) => {
+      if (key !== "username") localStorage.removeItem(key);
+    });
+  }, []);
+
+  return (
+    <Form>
+      <UserNameInput userName={userName} setUserName={setUserName} />
+      <QuestionSetInput
+        questionSetId={questionSetId}
+        setQuestionSetId={setQuestionSetId}
+      />
+      <Button variant="secondary" id="play-button" onClick={postStorage}>
+        Play
+      </Button>
+    </Form>
+  );
+}
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,65 +1,20 @@
-import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-
-// CSS関連
 import "./style.css";
-import { Button, Form } from "react-bootstrap";
 
 // コンポーネント
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import typingGameQuestionSets from "@typing-game/question-sets";
-import HowToPlay from "../features/game-setup/components/HowToPlay";
-import UserNameInput from "../features/game-setup/components/UserNameInput";
-import QuestionSetInput from "../features/game-setup/components/QuestionSetInput";
+
+import HowToPlay from "../features/game-setup/components/setup-form/components/HowToPlay";
+import SetupForm from "../features/game-setup/components/setup-form";
 
 export default function Home() {
-  const [userName, setUserName] = useState<string>(
-    localStorage.getItem("username") || "",
-  );
-  const [questionSetId, setQuestionSetId] = useState<string>(
-    typingGameQuestionSets[0].id,
-  );
-  // useNavigate を Navigate に変化させる呪文
-  const Navigate = useNavigate();
-
-  // ユーザーの入力情報を受け取る関数
-  const postStorage = () => {
-    localStorage.setItem("username", userName || "Guest");
-    localStorage.setItem("questionSetId", questionSetId);
-    // fetchAPI後に別ページへ遷移
-    Navigate("/basic");
-  };
-
-  // localStorageをリセットする
-  let unmounted = false;
-  useEffect(() => {
-    if (unmounted) return;
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    unmounted = true;
-    Object.keys(localStorage).forEach((key) => {
-      if (key !== "username") localStorage.removeItem(key);
-    });
-  }, []);
-
   return (
     <>
       <Header />
-
       <div className="elements">
-        <Form>
-          <UserNameInput userName={userName} setUserName={setUserName} />
-          <QuestionSetInput
-            questionSetId={questionSetId}
-            setQuestionSetId={setQuestionSetId}
-          />
-          <Button variant="secondary" id="play-button" onClick={postStorage}>
-            Play
-          </Button>
-        </Form>
+        <SetupForm />
         <HowToPlay />
       </div>
-
       <Footer />
     </>
   );
